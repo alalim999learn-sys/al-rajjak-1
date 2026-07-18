9675cb9e2f18450e337539ca19ed3d0d7ebd8cad
AL-RAJJAK <alalim999learn@gmail.com>
2026-07-18 14:06:46 +0600

    ALLAH

---
+++ app/ireland/cafe/page.tsx
@@ -0,0 +1,17 @@
+//C:\Users\Shanon\al-rajjak-1\app\ireland\cafe\page.tsx
+
+
+
+// অবশ্যই 'use client' অথবা সাধারণ কম্পোনেন্ট হিসেবে এভাবে থাকতে হবে
+'use client';
+
+import React from 'react';
+
+export default function CafePage() {
+  return (
+    <div>
+      <h1>Welcome to our Cafe</h1>
+      {/* আপনার কোড */}
+    </div>
+  );
+}
---
+++ app/ireland/grocery/page.tsx
@@ -0,0 +1,17 @@
+//C:\Users\Shanon\al-rajjak-1\app\ireland\grocery\page.tsx
+
+
+
+// অবশ্যই 'use client' অথবা সাধারণ কম্পোনেন্ট হিসেবে এভাবে থাকতে হবে
+'use client';
+
+import React from 'react';
+
+export default function GroceryPage() {
+  return (
+    <div>
+      <h1>Welcome to our Cafe</h1>
+      {/* আপনার কোড */}
+    </div>
+  );
+}
